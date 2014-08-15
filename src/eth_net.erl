74ac4830052814eef1af2c766a19541f0c3900cb
Tony Rogvall <tony@rogvall.se>
2014-08-15 16:18:28 +0200

    started adding udp and socket support

--- src/eth_net.erl
+++ src/eth_net.erl
@@ -2,11 +2,11 @@
 %%% @author Tony Rogvall <tony@rogvall.se>
 %%% @copyright (C) 2014, Tony Rogvall
 %%% @doc
-%%%    Eth arp daemon
+%%%    Eth tiny network stack
 %%% @end
 %%% Created : 10 Aug 2014 by Tony Rogvall <tony@rogvall.se>
 %%%-------------------------------------------------------------------
--module(eth_arpd).
+-module(eth_net).
 
 -behaviour(gen_server).
 
@@ -14,6 +14,10 @@
 -export([start/1]).
 -export([start_link/1]).
 -export([stop/1, add_ip/3, del_ip/2, find_mac/2, query_mac/2]).
+
+%% UDP
+-export([udp_open/4, udp_close/2, udp_send/5]).
+
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -29,6 +33,24 @@
 -define(BROADCAST, {16#ff,16#ff,16#ff,16#ff,16#ff,16#ff}).
 -define(ZERO,      {16#00,16#00,16#00,16#00,16#00,16#00}).
 
+-type socket_key() :: 
+	%% udp-unconnected | tcp-listen
+	{ ip_proto(), ip_address(), port_no() } | 
+	%% tcp-session | udp-connected
+	{ ip_proto(), ip_address(), port_no(), ip_address(), port_no() }.
+
+-record(socket,
+	{
+	  ref :: reference(),
+	  mac :: ethernet_address(),
+	  src_ip :: ip_address(),
+	  dst_ip :: ip_address(),
+	  src_port :: port_no(),
+	  dst_port :: port_no(),
+	  proto :: tcp | udp,
+	  owner :: pid()
+	}).
+
 -record(state,
 	{
 	  name :: string(),  %% name of interface (like "tap0", "en1", "eth0")
@@ -36,7 +58,9 @@
 	  mac :: ethernet_address(),       %% the mac address on interface
 	  ipmac :: dict:dict(ip_address(),ethernet_address()),
 	  macs :: sets:set(ethernet_address()),
-	  cache :: dict:dict(ip_address(),ethernet_address())
+	  cache :: dict:dict(ip_address(),ethernet_address()),
+	  sockets :: dict:dict(socket_key(), #socket{}),
+	  sockref :: dict:dict(reference(), socket_key())
 	 }).
 
 %%%===================================================================
@@ -44,10 +68,13 @@
 %%%===================================================================
 
 test() ->  %% run as root!
-    {ok,A} = start("tap"),
+    {ok,Net} = start("tap"),
     "" = os:cmd("ifconfig tap0 192.168.10.1 up"),
-    add_ip(A, {192,168,10,10}, {1,2,3,4,5,6}),
-    {ok,A}.
+    add_ip(Net, {192,168,10,10}, {1,2,3,4,5,6}),
+
+%%    {ok,U} = udp_open(Net, {192,168,10,10}, 6666, []),
+%%    udp_send(Net, U,  {192,168,10,1}, 6666, <<"Hello">>),
+    {ok,Net}.
 
 start(Interface) ->
     application:ensure_all_started(eth),
@@ -58,6 +85,18 @@ start_link(Interface) when is_list(Interface) ->
 
 stop(Arpd) ->
     gen_server:call(Arpd, stop).
+
+%% udp interface
+udp_open(Net, SrcIP, SrcPort, Options) ->
+    gen_server:call(Net, {udp_open, self(), SrcIP, SrcPort, Options}).
+
+udp_close(Net, UdpRef) ->
+    gen_server:call(Net, {udp_close, UdpRef}).
+
+udp_send(Net, UdpRef, DestIP, DestPort, Data) ->
+    gen_server:call(Net, {udp_send, UdpRef, DestIP, DestPort, Data}).
+
+
 
 add_ip(Arpd, IP, Mac) when (tuple_size(IP) =:= 4 orelse tuple_size(IP) =:= 8) 
 			   andalso (tuple_size(Mac) =:= 6) ->
@@ -77,9 +116,13 @@ query_mac(Arpd, IP) ->
 init([Interface]) ->
     case eth_devices:open(Interface) of
 	{ok,Port} ->
+	    %% to be used for "real" we should be more selective
 	    FilterProg = eth_bpf:build_programx(
-			   {'||', "ether.type.arp",
-			    {'&&', "ether.type.ip","ip.proto.icmp"}}),
+			   {'||', 
+			    ["ether.type.arp",
+			     {'&&',"ether.type.ip","ip.proto.icmp"},
+			     {'&&', "ether.type.ip","ip.proto.udp"},
+			     {'&&', "ether.type.ip","ip.proto.tcp"}]}),
 	    Filter = bpf:asm(FilterProg),
 	    _Reply0 = eth:set_filter(Port, Filter),
 	    _Reply1 = eth:set_active(Port, -1),
@@ -91,7 +134,9 @@ init([Interface]) ->
 			  mac = Mac, 
 			  ipmac = dict:new(),
 			  macs = sets:new(),
-			  cache = dict:new()
+			  cache = dict:new(),
+			  sockets = dict:new(),
+			  sockref = dict:new()
 			}};
 	Error ->
 	    {stop, Error}
@@ -111,6 +156,59 @@ init([Interface]) ->
 %%                                   {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
+
+handle_call({udp_open,Owner,SrcIP,SrcPort,_Options}, _From, State) ->
+    Key = {udp,SrcIP,SrcPort},
+    case dict:find(Key, State#state.sockets) of
+	error ->
+	    case dict:find(SrcIP, State#state.ipmac) of
+		error ->
+		    {reply, {error, einval}, State};
+		{ok,SrcMac} ->
+		    Ref = erlang:monitor(process, Owner),
+		    Socket = #socket { ref=Ref, mac = SrcMac,
+				       src_ip = SrcIP,
+				       src_port = SrcPort,
+				       proto = udp, owner = Owner },
+		    Sockets = dict:store(Key, Socket, State#state.sockets),
+		    SockRef = dict:store(Ref, Key, State#state.sockref),
+		    State1 =  State#state { sockets=Sockets, sockref=SockRef},
+		    {reply, {ok,Ref}, State1}
+	    end;
+	{ok,_Socket} ->
+	    {reply, {error,ealready}, State}
+    end;
+handle_call({udp_close,Ref}, _From, State) ->
+    case dict:find(Ref, State#state.sockref) of
+	error ->
+	    {reply, {error, enoent},  State};
+	{ok, SockKey} ->
+	    {ok, Socket} = dict:find(SockKey, State#state.sockets),
+	    ?dbg("closing socket ~p\n", [Socket]),
+	    SockRef = dict:erase(Ref, State#state.sockref),
+	    Sockets = dict:erase(SockKey, State#state.sockets),
+	    true = erlang:demonitor(Ref, [flush]),
+	    {reply, ok, State#state { sockref = SockRef, 
+				      sockets =Sockets}}
+    end;
+handle_call({udp_send,Ref,DstIP,DstPort,Data}, _From, State) ->
+    case dict:find(Ref, State#state.sockref) of
+	error ->
+	    {reply, {error, einval},  State};
+	{ok, SockKey} ->
+	    {ok, Socket} = dict:find(SockKey, State#state.sockets),
+	    Udp = #udp { src_port = Socket#socket.src_port,
+			 dst_port = DstPort,
+			 data = Data },
+	    Ip  = #ipv4 { src=Socket#socket.src_ip,
+			  dst=DstIP, proto=udp, data=Udp },
+	    DstMac = cache_lookup(DstIP, State),
+	    Eth = #eth { src=Socket#socket.mac, dst=DstMac,
+			 type=ipv4,data=Ip },
+	    send_frame(Eth, State),
+	    {reply, ok, State}
+    end;
+
 handle_call({add_ip,IP,Mac}, _From, State) ->
     IPMac = dict:store(IP, Mac, State#state.ipmac),
     Macs = sets:add_element(Mac, State#state.macs),
@@ -180,6 +278,23 @@ handle_info({eth_frame,_Port,_IfIndex,Data}, State) ->
 		      [Reason,erlang:get_stacktrace()]),
 	    {noreply, State}	    
     end;
+handle_info({'DOWN', Ref, process, _Pid, _Reason}, State) ->
+    case dict:find(Ref, State#state.sockref) of
+	error ->
+	    {noreply, State};
+	{ok, SockKey} ->
+	    case dict:find(SockKey, State#state.sockets) of
+		error ->
+		    {noreply, State};
+		{ok,Socket} ->
+		    ?dbg("closing socket ~p\n", [Socket]),
+		    SockRef = dict:erase(Ref, State#state.sockref),
+		    Sockets = dict:erase(SockKey, State#state.sockets),
+		    {noreply, State#state { sockref = SockRef, 
+					    sockets =Sockets}}
+	    end
+    end;
+
 handle_info(_Info, State) ->
     ?dbg("got info: ~p\n", [_Info]),
     {noreply, State}.
@@ -266,14 +381,12 @@ send_frame(Frame, State) ->
     Data=enet_eth:encode(Frame, []),
     eth_devices:send(State#state.eth, Data).
 
-
 get_mac_address(Interface) ->
     {ok, IfList} =  inet:getifaddrs(),
     case lists:keyfind(Interface, 1, IfList) of
 	false -> undefined;
 	{_,Fs} -> list_to_tuple(proplists:get_value(hwaddr,Fs))
     end.
-
 
 %% build for 
 insert_cache(_, {0,0,0,0,0,0}, State) -> State;
@@ -282,3 +395,9 @@ insert_cache({0,0,0,0,0,0,0,0}, _, State) -> State;
 insert_cache(Ip, Mac, State) ->
     IpMac = dict:store(Ip, Mac, State#state.cache),
     State#state { cache = IpMac }.
+
+cache_lookup(IP, State) ->
+    case dict:find(IP, State#state.cache) of
+	{ok,Mac} -> Mac;
+	error -> ?BROADCAST   %% signal that arp is needed (or route)
+    end.
